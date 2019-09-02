6610b7872968a1ab81d662167fc213cc9d38e292
ambat25 <25ambat25@gmail.com>
2019-09-02 14:23:21 +0100

    initial commit

---
+++ sw.js
@@ -0,0 +1,18 @@
+const cacheName = 'ambat-dev';
+const filesToCache = ['./', './index.html', './css/main.min.css'];
+
+self.addEventListener('install', function(e) {
+	e.waitUntil(
+		caches.open(cacheName).then(function(cache) {
+			return cache.addAll(filesToCache);
+		})
+	);
+});
+
+self.addEventListener('fetch', function(e) {
+	e.respondWith(
+		caches.match(e.request).then(function(response) {
+			return response || fetch(e.request);
+		})
+	);
+});
